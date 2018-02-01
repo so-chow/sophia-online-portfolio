84dfc855196f7eda79b80919bb3925bdae6971b6
Sophia Chow <Sophia.t.chow@gmail.com>
2018-01-31 20:44:03 -0600

    added hover effects

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-//Dependencies
-const express = require('express');
+// //Dependencies
+// const express = require('express');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-//App Setup
-const app = express();
-const port = process.env.PORT || 3000;
+// //App Setup
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-//Routes
-app.get('/', (req, res) =>{
-  res.render('index');
-});
+// //Routes
+// app.get('/', (req, res) =>{
+//   res.render('index');
+// });
 
-//App Start
-app.listen(port, (err) => {
-  if(err){
-    console.log(`Error: ${err}`)
-  } else {
-    console.log(`Server running on port ${port}`);
-  }
-});
+// //App Start
+// app.listen(port, (err) => {
+//   if(err){
+//     console.log(`Error: ${err}`)
+//   } else {
+//     console.log(`Server running on port ${port}`);
+//   }
+// });
